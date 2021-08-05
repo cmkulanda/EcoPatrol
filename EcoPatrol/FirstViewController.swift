08fffee2775c61adca380da04b6d2d02886a3351
Kelliesu <Kelliesucha5@gmail.com>
2021-08-04 21:45:40 -0500

    Added View Controllers(Not the files though)

---
+++ EcoPatrol/FirstViewController.swift
@@ -0,0 +1,31 @@
+//
+//  FirstViewController.swift
+//  EcoPatrol
+//
+//  Created by Kellie Sucha on 8/4/21.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
