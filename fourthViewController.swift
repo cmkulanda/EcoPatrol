884089ff047233ddbb99c5f1f8c065d31b4948fa
Christa Kulanda <christakulanda@gmail.com>
2021-08-05 20:12:05 -0500

    report page complete - finished adding paige's code pt.2

--- fourthViewController.swift
+++ fourthViewController.swift
@@ -7,23 +7,50 @@
 
 import UIKit
 
-class fourthViewController: UIViewController {
+class fourthViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var location: UILabel!
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var quantity: UILabel!
+    @IBOutlet weak var colors: UILabel!
+    @IBOutlet weak var height: UILabel!
+    @IBOutlet weak var displayImage: UIImageView!
+    
+    var imagePicker = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.delegate = self
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func submitSightingTapped(_ sender: UIButton) {
     }
-    */
+    
+    @IBAction func takePhotoTapped(_ sender: UIButton) {
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated : true, completion : nil)
+
+
+    }
+    
+    @IBAction func photoLibraryTapped(_ sender: UIButton) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated : true, completion : nil)
+
+    }
+   
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+           
+           if let selectedImage = info [UIImagePickerController.InfoKey.originalImage] as? UIImage {
+               displayImage.image = selectedImage
+           }
+           imagePicker.dismiss(animated: true, completion: nil)
+       }
+
+    
+    
+    
 
 }
