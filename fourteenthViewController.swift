18ab7b6006ca5f04b30c651405af4dbe6b6a61cf
Christa Kulanda <christakulanda@gmail.com>
2021-08-06 10:42:29 -0500

    added tips page

---
+++ fourteenthViewController.swift
@@ -0,0 +1,35 @@
+//
+//  fourteenthViewController.swift
+//  EcoPatrol
+//
+//  Created by Scholar on 8/6/21.
+//
+
+import UIKit
+
+class fourteenthViewController: UIViewController {
+
+    @IBOutlet weak var nameOne: UILabel!
+    @IBOutlet weak var linkOne: UILabel!
+    @IBOutlet weak var nameTwo: UILabel!
+    @IBOutlet weak var linkTwo: UILabel!
+    @IBOutlet weak var nameThree: UILabel!
+    @IBOutlet weak var linkThree: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ thirteenthViewController.swift
@@ -0,0 +1,36 @@
+//
+//  thirteenthViewController.swift
+//  EcoPatrol
+//
+//  Created by Scholar on 8/6/21.
+//
+
+import UIKit
+
+class thirteenthViewController: UIViewController {
+
+    @IBOutlet weak var nameOne: UILabel!
+    @IBOutlet weak var numberOne: UILabel!
+    @IBOutlet weak var numberTwo: UILabel!
+    @IBOutlet weak var numberThree: UILabel!
+    @IBOutlet weak var numberFour: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func clickHere(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
