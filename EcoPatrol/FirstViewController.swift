79cc79be25d1da3da48be2d016a39edc2a6bc912
Christa Kulanda <christakulanda@gmail.com>
2021-08-05 19:23:41 -0500

    added paige's code in for the states feature :)

--- EcoPatrol/FirstViewController.swift
+++ EcoPatrol/FirstViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
     @IBOutlet weak var welcomeLabel: UILabel!
     @IBOutlet weak var loginMessage: UILabel!
     @IBOutlet weak var username: UILabel!
@@ -16,27 +16,66 @@ class FirstViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var createAccountMessage: UILabel!
     @IBOutlet weak var state: UILabel!
+    var selectedCountry: String?
+    var countryList = [" ", "Illinois", " "]
     @IBOutlet weak var stateTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        createPickerView()
+        dismissPickerView()
+        
     }
+        
+        func numberOfComponents(in pickerView: UIPickerView) -> Int {
+                return 1
+            }
+            
+            func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+                return countryList.count
+            }
+            
+            func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+                return countryList[row]
+               
+            }
+            
+            func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+                selectedCountry = countryList[row]
+                stateTextField.text = selectedCountry
+            }
+            
+            func createPickerView() {
+                let pickerView = UIPickerView()
+                pickerView.delegate = self
+                stateTextField.inputView = pickerView
+            }
+            
+            func dismissPickerView() {
+                let toolBar = UIToolbar()
+                toolBar.sizeToFit()
+                
+                let button = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(self.action))
+                toolBar.setItems([button], animated: true)
+                toolBar.isUserInteractionEnabled = true
+                stateTextField.inputAccessoryView = toolBar
+            }
+            
+            @objc func action() {
+               view.endEditing(true)
+            }
+         
+
+        
+        
+        
+        
+        
+        
     
     @IBAction func `continue`(_ sender: UIButton) {
     }
     
     
     
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
